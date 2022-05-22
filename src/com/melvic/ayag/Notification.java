c28d008d6c87a76a5b9e30891073a88ff679203d
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2022-05-22 22:37:43 +0800

    Make notification mac or linux specific

--- src/com/melvic/ayag/Notification.java
+++ src/com/melvic/ayag/Notification.java
@@ -4,19 +4,23 @@ import java.io.IOException;
 
 public class Notification {
     /**
-     * Displays Mac notification
+     * Displays Mac or Linux specific notification
      */
     public static void display() {
-        String title = "Ayag";
+        String title = "Ayag";  // TODO: Make this title configurable
         String message = "Time is up!";
         Runtime runtime = Runtime.getRuntime();
         String os = System.getProperty("os.name");
-        String[] code;
+        String[] code = {};
 
         if (os.contains("Mac")) {
-            code = new String[]{"osascript", "-e", "display notification \"" + message + "\" with title \"" + title + "\""};
-        } else {
-            code = new String[]{"zenity", "--notification", "--title=" + title, "--text=" + message};
+            code = new String[]{
+                    "osascript", "-e", "display notification \"" + message + "\" with title \"" + title + "\""
+            };
+        } else if (os.contains("Linux")) {
+            code = new String[]{
+                    "zenity", "--notification", "--title=" + title, "--text=" + message
+            };
         }
 
         try {
